036d2d2d9391b2667f95d953b7ba7c66fe8ff8b4
Moshiur-15 <masiurislam28@gmail.com>
2025-01-28 22:08:07 +0600

    readme.md

--- src/Components/Dashboard/participantNav.jsx
+++ src/Components/Dashboard/participantNav.jsx
@@ -11,12 +11,13 @@ const ParticipantNav = () => {
     <div>
       <div className="text-gray-400 font-semibold">Participant</div>
       <div className="space-y-2 mt-2">
-        <NavLinks to="/dashboard/analytics" name="Analytics" social={<HiChartBar />} />
+        
         <NavLinks
           to="/dashboard/registered-camps"
           name="Registered Camps"
           social={<HiOutlineClipboardList />}
         />
+        <NavLinks to="/dashboard/analytics" name="Analytics" social={<HiChartBar />} />
         <NavLinks
           to="/dashboard/payment-history"
           name="Payment History"
